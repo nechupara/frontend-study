d6d8e74c0326d12e12b705192488561018e5216c
Serhii Dudko <nechupara@gmail.com>
2021-11-23 01:29:39 +0200

    hw-22 done

---
+++ homeworks/02-js-basics/hw22-colored_table/script.js
@@ -0,0 +1,21 @@
+const table = document.createElement('table');
+
+for (let i = 1; i <= 30; i++) {
+    const row = document.createElement('tr');
+    for (let i = 1; i <= 30; i++) {
+        row.insertAdjacentHTML('beforeend','<td></td>');
+    }
+    table.append(row);
+}
+
+document.body.prepend(table);
+
+table.addEventListener('click', event => {
+    event.stopPropagation();
+    if (event.target.tagName !== 'TD') return;
+    event.target.classList.toggle('black');
+})
+
+document.body.addEventListener('click', () => {
+    table.classList.toggle('reverse')
+})
