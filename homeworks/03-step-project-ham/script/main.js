095898934cba89570f98ef4ac5de8861a0cc950a
Serhii Dudko <nechupara@gmail.com>
2022-01-16 15:43:41 +0200

    in progress

--- homeworks/03-step-project-ham/script/main.js
+++ homeworks/03-step-project-ham/script/main.js
@@ -13,3 +13,81 @@ servicesTabs.addEventListener("click", (e) => {
         document.querySelector(`.service-tab-article[data-service-title='${title}']`).classList.add("visible");
     }
 });
+
+// Our Amazing Work
+const filterTabs = document.querySelector('.filter-list');
+
+filterTabs.addEventListener('click', e => {
+    const clickedFilter = e.target;
+    if (!clickedFilter.classList.contains('filter-tab')) return;
+
+    if (!clickedFilter.classList.contains('active')) {
+        filterTabs.querySelector('.active').classList.remove('active');
+
+        clickedFilter.classList.add('active');
+    }
+
+});
+
+
+const makeHoverHTML = (filterCatgory) => {
+    const categoryNames = {
+        'graphic-design': 'Graphic Design',
+        'web-design': 'Web Design',
+        'landing-pages': 'Landing Pages',
+        'wordpress': 'Wordpress',
+    }
+
+    const categoryName = categoryNames[filterCatgory] ? categoryNames[filterCatgory] : 'NA';
+    const htmlContent = `
+        <div class="work-img-hover">
+            <div class="work-img-hover-circles">
+                <div class="img-hover-circle chain">
+                </div>
+                <div class="img-hover-circle square">
+                    <div class="square"></div>
+                </div>
+            </div>
+            <span class="work-img-text-colored upper">creative design</span>
+            <span class="work-img-text-grey">${categoryName}</span>
+        </div>
+    `
+    return htmlContent;
+}
+
+const workImages = document.querySelector('.work-imgs');
+let currentHoveredElement = null; // test
+
+workImages.addEventListener('mouseover', e => {
+    const hoveredElem = e.target.closest('.work-img-wrapper');
+
+    if (currentHoveredElement === hoveredElem) return;
+    if (currentHoveredElement && currentHoveredElement !== hoveredElem) {
+        currentHoveredElement.querySelector('.work-img-hover').remove();
+    }
+    currentHoveredElement = hoveredElem;
+    if (!hoveredElem.querySelector('.work-img-hover')) {
+        
+        console.log(hoveredElem);
+        console.log(hoveredElem.dataset.workCategory);
+        hoveredElem.insertAdjacentHTML('beforeend', makeHoverHTML(hoveredElem.dataset.workCategory));
+
+    }
+    
+});
+
+workImages.addEventListener('mouseout', event => {
+    if (event.relatedTarget === null && currentHoveredElement) {
+        currentHoveredElement.querySelector('.work-img-hover').remove();
+        currentHoveredElement = null;
+        return;
+    }
+    const relatedTarget = event.relatedTarget.closest('.work-img-wrapper');
+    if (relatedTarget !== null && relatedTarget === currentHoveredElement) return;
+    const leftIMG = event.target.closest('.work-img-wrapper')
+    // if (leftIMG.querySelector('.work-img-hover')) {
+        leftIMG.querySelector('.work-img-hover').remove();
+        currentHoveredElement = null;
+        console.log('leeeeeeee');
+    // }
+});
