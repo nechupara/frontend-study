aea05f238e0b46fd617eed27be89589de18d839f
Serhii Dudko <nechupara@gmail.com>
2022-02-02 17:24:54 +0200

    before hw-02 gulp scss

--- test/masonry/main.js
+++ test/masonry/main.js
@@ -1,15 +1,15 @@
 const elem = document.querySelectorAll('.grid');
 
-const masonry = new Masonry (elem[0], {
-    itemSelector: '.grid-item',
-    columnWidth: 160,
-    gutter: 10,
-  });
+const masonry = new Masonry(elem[0], {
+        itemSelector: '.grid-item',
+        columnWidth: 160,
+        gutter: 10,
+});
 
-  const masonry2 = new Masonry (elem[1], {
-    itemSelector: '.grid-item',
-    columnWidth: 180
-  });
+const masonry2 = new Masonry(elem[1], {
+        itemSelector: '.grid-item',
+        columnWidth: 180,
+});
 
 // $('.grid').masonry({
 //     itemSelector: '.grid-item',
